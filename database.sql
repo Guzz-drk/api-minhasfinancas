1abaf4b901486072f4401f525362d94d2f7cc7f1
Gustavo Pandolpho <gwprkg@gmail.com>
2023-04-12 22:21:07 -0300

    feat: inicio_projeto

---
+++ database.sql
@@ -0,0 +1,26 @@
+﻿
+CREATE DATABASE minhasfinancas;
+
+CREATE SCHEMA financas;
+
+CREATE TABLE financas.usuario
+(
+  id bigserial NOT NULL PRIMARY KEY,
+  nome varchar(150),
+  email varchar(100),
+  senha varchar(20),
+  data_cadastro date default now()
+);
+
+CREATE TABLE financas.lancamento
+(
+  id bigserial NOT NULL PRIMARY KEY ,
+  descricao varchar(100) NOT NULL,
+  mes integer NOT NULL,
+  ano integer NOT NULL,
+  valor numeric(16,2),
+  tipo varchar(20) check (tipo in ('RECEITA', 'DESPESA')) NOT NULL,
+  status varchar(20) check (status in ('PENDENTE', 'CANCELADO', 'EFETIVADO')) NOT NULL,
+  id_usuario bigint REFERENCES financas.usuario (id),
+  data_cadastro date default now()
+);
